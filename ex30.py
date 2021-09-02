34dc41e8b2fdade5466907f9fd5fde759d1bf1e3
Yasmin Michels <yamichels@gmail.com>
2021-09-02 14:29:26 -0300

    ex30
    exercicio de numero 30

---
+++ ex30.py
@@ -0,0 +1,4 @@
+real = float(input("Digite o valor em reais: "))
+cotacao = float(input("Digite a cotação do dólar: "))
+valordol = (real / cotacao)
+print("O valor digitado convertido em dolar fica:",valordol)
